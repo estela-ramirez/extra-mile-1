b4436dcf3e29493d30b8cb2dd0d02544d0fc6a4e
Estela Ramirez <ramirezestela01@gmail.com>
2022-09-02 13:23:10 -0700

    make it better

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require ('path');
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["user1", "user2", "user3"]});
+    res.json({"users": ["user1", "user2", "user4"]});
 })
 // so server can parse through incoming json & limit amount per request
 app.use(express.json({limit: '1mb'}));  
@@ -12,7 +12,7 @@ app.use(express.json({limit: '1mb'}));
 const PORT = process.env.PORT || 5000; // backend routing port
 
 if (process.env.NODE_ENV == "production") {
-    app.use(express.static(path.join(__dirname + '/public')));
+    app.use(express.static(path.join(__dirname + '/build')));
     
 }
 
