cbcc1b508c298bf386a08c239f6028d69c62b410
Estela Ramirez <ramirezestela01@gmail.com>
2022-09-02 13:09:48 -0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const path = require ('path');
+
+app.get("/api", (req, res) => {
+    res.json({"users": ["user1", "user2", "user3"]});
+})
+// so server can parse through incoming json & limit amount per request
+app.use(express.json({limit: '1mb'}));  
+
+const PORT = process.env.PORT || 5000; // backend routing port
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname + '/public')));
+    
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
