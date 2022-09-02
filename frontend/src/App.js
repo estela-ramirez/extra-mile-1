a057f30589245d9a0e962b943fa892ccd4a2e6ef
Estela Ramirez <ramirezestela01@gmail.com>
2022-09-02 14:09:31 -0700

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,22 @@ import React, { useEffect, useState} from 'react'
 function App() {
     const [data, setData] = useState([{}]);
 
-    useEffect(()=>{
-        fetch('/api')
-        .then(response => response.json())
-        .then((json) => { 
-            setData(json);
-        });
-    }, [])
+    // useEffect(()=>{
+    //     fetch('/api')
+    //     .then(response => response.json())
+    //     .then((json) => { 
+    //         setData(json);
+    //     });
+    // }, [])
 
   return (
     <div className="App">
-      {(typeof data.users == "undefined") ? (
+      <p>Hello World</p>
+      {/* {(typeof data.users == "undefined") ? (
         <p>Loading ....</p>
       ) : data.users.map((users, k) => (
         <p key = {k}>{users}</p>
-      ))}
+      ))} */}
     </div>
   );
 }
